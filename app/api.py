2d0bf4f621515e93ff6b865de1ae22af13edb516
chado13 <geaya13@gmail.com>
2024-02-10 13:25:08 +0900

    feat: future candle api 추가

--- app/api.py
+++ app/api.py
@@ -7,7 +7,7 @@ from app.config import config
 from app.constants import FreqEnum
 
 
-def get_candle_data(
+def fetch_candle_data(
     symbol: str,
     interval: FreqEnum,
     start: datetime | None = None,
@@ -24,5 +24,26 @@ def get_candle_data(
         "limit": limit,
     }
     path = f"{config.binance_base_url}/api/v3/klines"
+    res = requests.get(path, params)
+    return res.json()
+
+
+def fetch_future_candle_data(
+    symbol: str,
+    interval: FreqEnum,
+    start: datetime | None = None,
+    end: datetime | None = None,
+    time_zone: int = 0,
+    limit: int = 500,
+) -> list[list[Any]]:
+    params = {
+        "symbol": symbol,
+        "interval": interval,
+        "startTime": start.timestamp() if start else start,
+        "endTime": end.timestamp() if end else end,
+        "timeZone": time_zone,
+        "limit": limit,
+    }
+    path = f"{config.binance_future_url}/fapi/v1/klines"
     res = requests.get(path, params)
     return res.json()
--- app/config.py
+++ app/config.py
@@ -1,11 +1,13 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
+
 class Config(BaseSettings):
-    model_config = SettingsConfigDict(env_file='secrets/.env')
+    model_config = SettingsConfigDict(env_file="secrets/.env")
 
     binance_key: str
     binance_secret: str
     binance_base_url: str
+    binance_future_url: str
 
 
 config = Config()
